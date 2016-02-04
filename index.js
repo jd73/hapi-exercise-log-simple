befcac6aa4ab48edd5ce9a4797de9f143247cbac
Joseph Downing <joseph.downing@gmail.com>
2016-02-04 00:09:13 -0600

    Refactor into plugin

--- index.js
+++ index.js
@@ -3,43 +3,17 @@
 const Hapi = require('hapi');
 const Blipp = require('blipp');
 
-const ExerciseRepository = require('./lib/repository/exercise_repository');
-const Exercise = require('./lib/models/exercise');
-const ExerciseController = require('./lib/controllers/exercise');
+const Exercise = require('./lib/exercise/index');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.register([Blipp], (err) => {
+server.register([
+  Blipp,
+  Exercise
+], (err) => {
   if (err) {
     console.error('Failed to load a plugin:', err);
-  }
-});
-
-const repository = new ExerciseRepository();
-
-server.route({
-  method: 'GET',
-  path: '/',
-  config: {
-    bind: {
-      repository: repository
-    },
-    handler: ExerciseController.list
-  }
-});
-
-server.route({
-  method: 'POST',
-  path: '/',
-  config: {
-    bind: {
-      repository: repository
-    },
-    validate: {
-      payload: Exercise
-    },
-    handler: ExerciseController.save
   }
 });
 
